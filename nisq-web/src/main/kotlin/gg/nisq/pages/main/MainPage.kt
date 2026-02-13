0b7cf5666fb641a8b014bba26a242b668193fa68
Martin Lehtomaa <martin.lehtomaa@gmail.com>
2026-02-13 17:44:46 +0200

    Added daisy UI and tailwind CSS

--- nisq-web/src/main/kotlin/gg/nisq/pages/main/MainPage.kt
+++ nisq-web/src/main/kotlin/gg/nisq/pages/main/MainPage.kt
@@ -4,15 +4,12 @@ import kotlinx.html.*
 import layout
 
 fun HTML.mainPage() = layout {
-
     div(classes = "flex") {
-
         div(classes = "flex-none w-14 h-14 bg-red-800") {
             h1(classes = "bg-red-800") {
                 +"Side panel"
             }
         }
-
         div(classes = "flex-auto w-64 bg-amber-600") {
             h1(classes = "") {
                 +"Nippon squad Main"
@@ -23,7 +20,6 @@ fun HTML.mainPage() = layout {
                 }
             }
         }
-
         div(classes = "flex-auto w-32 bg-orange-200") {
             h1(classes = "") {
                 +"Side panel"
